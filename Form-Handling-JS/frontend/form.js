1e5e76d563790d514b783eceae35fe5968f1c630
Vaishali <2004sharmaanshika@gmail.com>
2025-03-23 22:23:49 +0530

    fetch Countries using API

--- Form-Handling-JS/frontend/form.js
+++ Form-Handling-JS/frontend/form.js
@@ -1,16 +1,41 @@
-const getCountry = document.querySelector('.country');
-console.log("getCountry",getCountry)
+const selectCountry = document.querySelector("#countries");
+const selectState = document.querySelector("#inputState");
+const apiKey = 'aabbccdd';
 
-document.addEventListener('DOMContentLoaded',getAllCountries);
+document.addEventListener("DOMContentLoaded", getAllCountries);
+selectCountry.addEventListener("change",function() {
+    const countryCode = this.value;
+    console.log("countryCode: ",countryCode);
+    getStates(countryCode);
+})
 
 function getAllCountries() {
-    const selectDrop = document.querySelector('#countries');
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      selectCountry.innerHTML = '<option value="">Select Country</option>'; // Default option
+      console.log(data)
+      data.forEach((country) => {
+        let option = document.createElement("option");
+        option.value = country.cca2; // Country code as value
+        option.textContent = country.name.common; // Country name as text
+        selectCountry.appendChild(option);
+      });
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+}
 
-    fetch('https://restcountries.com/v3.1/all').then(res => {
-        return res.json();
-    }).then(data => {
-        console.log(data[0]);
-    }).catch(err => {
-        console.log("Error: ",err);
+function getStates(countryCode) {
+    selectState.innerHTML = '<option value="">Select State</option>';
+    fetch(`https://api.countrystatecity.in/v1/countries/${countryCode}/states`, {
+        headers: { "X-CSCAPI-KEY": apiKey }
+    })
+    .then(res => res.json())
+    .then((data) => {
+        console.log("data: ",data);
     })
 }
